32f61130b08350876e757bedf97daf395bf1bf3c
Dipen Patel <60754812+developingdipen@users.noreply.github.com>
2020-06-15 15:35:37 -0400

    Create script.js

---
+++ script.js
@@ -0,0 +1,19 @@
+function openNav() {
+    document.getElementById("myNav").style.width = "100%";
+  }
+  
+  function closeNav() {
+    document.getElementById("myNav").style.width = "0%";
+  }
+
+
+  var prevScrollpos = window.pageYOffset;
+window.onscroll = function() {
+var currentScrollPos = window.pageYOffset;
+  if (prevScrollpos > currentScrollPos) {
+    document.getElementById("navbar").style.top = "0";
+  } else {
+    document.getElementById("navbar").style.top = "-150px";
+  }
+  prevScrollpos = currentScrollPos;
+}
